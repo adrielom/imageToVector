f3a43c3f22647ce4eaf8d41c761457423c34d405
Adriel Oliveira <adriel.oliveira@projeto.lead>
2020-09-04 20:40:36 -0300

    tracing routes done

--- index.js
+++ index.js
@@ -9,52 +9,52 @@ const express = require('express')
 const PORT = process.env.PORT || 8080
 
 const app = express()
-let img = 'c80772f03893efae04c114a96c3cff49.jpg'
-let activeImage = {}
 
-const tracing = async (obj) => {
-    console.log(obj)
+const tracing = async (obj, call) => {
     let params = {
-        color: obj.color != 'black' ? obj.color : 'black',
+        color: obj.color === undefined ? `black` : obj.color,
         threshold: obj.threshold === undefined ? potrace.Potrace.THRESHOLD_AUTO : parseFloat(obj.threshold),
-        turdSize: obj.turdSize != 2 ? obj.turdSize : 2,
-        optTolerance: obj.optTolerance == 0.2 ? 0.2 : obj.optTolerance
+        turdSize: obj.turdSize === undefined ? 2 : obj.turdSize,
+        optTolerance: obj.optTolerance === undefined ? 0.2 : obj.optTolerance
     };
+    p = path.join(__dirname, `toVector`, `${obj.name}`)
 
-    p = path.join(__dirname, `./toVector/${obj.name}`)
-    console.log('color is: ' + params.color)
     potrace.trace(p, params, function (err, svg) {
         if (err) throw err;
 
         fs.writeFileSync(`./svgs/${obj.name}.svg`, svg);
+        call()
     });
+
 
 }
 
-const posterize = async (obj) => {
+const posterize = (obj) => {
     let params = {
         color: obj.color != 'black' ? obj.color : 'black',
-        steps: obj.thresholds === undefined || thresholds === [] ? [potrace.Potrace.THRESHOLD_AUTO] : obj.thresholds,
+        steps: obj.thresholds === undefined || obj.thresholds === [] ? [potrace.Potrace.THRESHOLD_AUTO] : obj.thresholds,
         turdSize: obj.turdSize != 2 ? obj.turdSize : 2,
         optTolerance: obj.optTolerance == 0.2 ? 0.2 : obj.optTolerance
     };
 
-    p = path.join(__dirname, `./toVector/${obj.name}`)
-    console.log("posterize is " + p)
+    p = path.join(__dirname, `toVector`, `${obj.name}`)
+
     potrace.trace(p, params, function (err, svg) {
         if (err) throw err;
 
         fs.writeFileSync(`./svgs/${obj.name}.svg`, svg);
+        call()
     });
 
 }
 
-function removeActiveImage(name) {
-    console.log('in remove function ' + name)
-    fs.exists(`./toVector/${name}`, (exists) => {
+function removeActiveImage(name, folder, extension) {
+    let fPath = `${path.join(__dirname, folder, name)}.${extension}`
+    console.log(fPath)
+    fs.exists(fPath, (exists) => {
         if (exists) {
             console.log('exists')
-            fs.unlink(`./toVector/${name}`, (err) => {
+            fs.unlink(fPath, (err) => {
                 if (err) console.log(err)
                 else console.log('worked')
             })
@@ -64,21 +64,23 @@ function removeActiveImage(name) {
 
 
 
-app.get('/posterize', async (req, res) => {
+app.get('/posterize', (req, res) => {
     let name = req.query.name
     let thresholds = req.query.thresholds
     let turdSize = req.query.turdSize
     let optTolerance = req.query.optTolerance
+    let color = req.query.color
 
-    await posterize({
+    posterize({
         name,
         thresholds,
         turdSize,
-        optTolerance
-    })
-    res.sendFile(path.join(__dirname + '/toVector' + `/${name}.svg`), (err) => {
+        optTolerance,
+        color
+    }, res.sendFile(path.join(__dirname + 'toVector' + `${name}.svg`), (err) => {
         console.log(err)
-    })
+    }))
+
 })
 
 
@@ -95,42 +97,32 @@ app.get('/tracing', async (req, res) => {
         turdSize,
         optTolerance,
         color
+    }, () => {
+        res.sendFile(path.join(__dirname + '/svgs' + `/${name}.svg`))
+        callback(name, 900000, 'svgs', 'svg')
     })
-    res.sendFile(path.join(__dirname + '/toVector' + `/${name}.svg`), (err) => {
-        console.log(err)
-    })
+
 })
 
 
 
 app.post('/imgToVector', upload.single('name'), (req, res) => {
-    console.log(req.file)
-    let options = {
-        root: path.join(__dirname, 'toVector'),
-        dotfiles: 'deny',
-        headers: {
-            'x-timestamp': Date.now(),
-            'x-sent': true
-        }
-    }
 
     let file = req.file
+    let activeImage = {}
 
     activeImage.name = file.filename
     activeImage.originalName = file.originalname
     activeImage.extension = file.originalname.split('.')[1]
     activeImage.file = file
-    console.log(activeImage)
-    callback(activeImage.name, 360000)
+    callback(activeImage.name, 900000, 'toVector')
     res.send(activeImage)
 })
 
 
-function callback(name, time) {
-    console.log(name)
+function callback(name, time, folder, extension) {
     setTimeout(() => {
-        console.log(time)
-        removeActiveImage(name)
+        removeActiveImage(name, folder, extension)
     }, time, name)
 
 }
